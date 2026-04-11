ecc666b589c29de466fcb0347c0d9597967965d5
supermanone-boop <daikihashi1127@gmail.com>
2026-04-11 14:07:18 +0900

    bookmarklet.js の更新

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,5 +1,5 @@
 javascript:(function(){
-  var url = prompt("https://raw.githubusercontent.com/supermanone-boop/add-aircraft-to-geofs/refs/heads/main/aircraft.json");
+  var url = prompt("https://raw.githubusercontent.com/supermanone-boop/add-aircraft-to-geofs/main/aircraft.json");
   if(url){
     geofs.aircraft.loadFromUrl(url);
   }
