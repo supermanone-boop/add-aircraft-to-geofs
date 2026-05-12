2eaabe2dde28de98c68ecc6bc2bdfa48b16af8cc
supermanone-boop <daikihashi1127@gmail.com>
2026-05-12 19:00:19 +0900

    b52.js の作成

---
+++ userscript.js
@@ -0,0 +1,43 @@
+'use strict';
+
+(() => {
+
+    const ac = geofs.aircraft.instance;
+
+    console.log("[B52 GLB Merge] init");
+
+    const GLB_URL =
+        "https://raw.githubusercontent.com/supermanone-boop/models/main/boeing_b-52_stratofortress.glb";
+
+    // -------------------------
+    // ① 初期非表示
+    // -------------------------
+    function hideNative() {
+        if (ac.parts?.root?.object3d) {
+            ac.parts.root.object3d.visible = false;
+        }
+    }
+
+    hideNative();
+
+    // -------------------------
+    // ② カメラ切替・再生成対策
+    // -------------------------
+    setInterval(hideNative, 300);
+
+    // -------------------------
+    // ③ GLB追加
+    // -------------------------
+    ac.addParts([
+        {
+            name: "b52_center",
+            model: GLB_URL,
+            rotation: [0, 0, 90],
+            position: [0, 0, 0],
+            scale: [1, 1, 1]
+        }
+    ]);
+
+    console.log("[B52 GLB Merge] added");
+
+})();
