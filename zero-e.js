130e8f70deda11c335f109b1731bbc2452f81568
supermanone-boop <daikihashi1127@gmail.com>
2026-05-20 17:45:43 +0900

    zero-e.js の作成

---
+++ zero-e.js
@@ -0,0 +1,44 @@
+'use strict';
+
+(() => {
+
+const ac = geofs.aircraft.instance;  
+
+console.log("[B52 GLB Merge] init");  
+
+const GLB_URL =  
+    "https://raw.githubusercontent.com/supermanone-boop/models/main/Airbus_ZeroE.glb";  
+
+// -------------------------  
+// ① 初期非表示  
+// -------------------------  
+function hideNative() {  
+    if (ac.parts?.root?.object3d) {  
+        ac.parts.root.object3d.visible = false;  
+    }  
+}  
+
+hideNative();  
+
+// -------------------------  
+// ② カメラ切替・再生成対策  
+// -------------------------  
+setInterval(hideNative, 300);  
+
+// -------------------------  
+// ③ GLB追加  
+// -------------------------  
+ac.addParts([  
+    {  
+        name: "b52_center",  
+        model: GLB_URL,  
+        rotation: [0, 0, 270],  
+        position: [0, 0, -100],  
+        scale: [0.1, 0.1, 0.1]  
+    }  
+]);  
+
+console.log("[B52 GLB Merge] added");
+
+})();
+airbus zero-eのコード
