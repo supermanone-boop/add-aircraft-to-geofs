db6e6cee90f1f77484fc205900c4b41576915fc4
supermanone-boop <daikihashi1127@gmail.com>
2026-05-20 17:44:31 +0900

    SR72.js の作成

---
+++ SR72.js
@@ -0,0 +1,44 @@
+'use strict';
+
+(() => {
+
+const ac = geofs.aircraft.instance;  
+
+console.log("[B52 GLB Merge] init");  
+
+const GLB_URL =  
+    "https://raw.githubusercontent.com/supermanone-boop/models/main/lockheed_martin_sr72_darkstar__topgun_maverick.glb";  
+
+// -------------------------  
+// ① 初期非表示  
+// -------------------------  
+function hideNative() {  
+    if (ac.parts?.root?.object3d) {  
+        ac.parts.root.object3d.visible = false;  
+    }  
+}  
+
+hideNative();  
+
+// -------------------------  
+// ② カメラ切替・再生成対策  
+// -------------------------  
+setInterval(hideNative, 300);  
+
+// -------------------------  
+// ③ GLB追加  
+// -------------------------  
+ac.addParts([  
+    {  
+        name: "b52_center",  
+        model: GLB_URL,  
+        rotation: [0, 0, 270],  
+        position: [0, 0, 0],  
+        scale: [4, 4, 4]  
+    }  
+]);  
+
+console.log("[B52 GLB Merge] added");
+
+})();
+SR72
